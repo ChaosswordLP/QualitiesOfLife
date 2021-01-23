b50c9cc408a00d4ddd1b4f7d339ea769e2bda3f8
ChaosswordLP <48867311+ChaosswordLP@users.noreply.github.com>
2021-01-23 15:33:33 +0100

    added Gamemodes

---
+++ src/Chaos/QualitiesOfLife/CommandExecutor/GamemodeSpectatorExecutor.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Chaos\QualitiesOfLife\CommandExecutor;
+
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\GameMode;
+use pocketmine\player\Player;
+
+class GamemodeSpectatorExecutor implements CommandExecutor {
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+		if(count($args) > 0) {
+			$player = $sender->getServer()->getPlayerByPrefix($args[0]);
+			if($player instanceof Player){
+				if($player->getGamemode() === GameMode::SPECTATOR()) {
+					return true;
+				}else{
+					$player->setGamemode(GameMode::SPECTATOR());
+				}
+			}else{
+				$sender->sendMessage($args[0]." §cis not online");
+			}
+		}else{
+			if($sender instanceof Player){
+				if($sender->getGamemode() === GameMode::SPECTATOR()){
+					return true;
+				}else{
+					$sender->setGamemode(GameMode::SPECTATOR());
+				}
+			}else{
+				$sender->sendMessage("Fuck off");
+			}
+		}
+		return true;
+	}
+}
---
+++ src/Chaos/QualitiesOfLife/CommandExecutor/GamemodeSurvivalExecutor.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Chaos\QualitiesOfLife\CommandExecutor;
+
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\GameMode;
+use pocketmine\player\Player;
+
+class GamemodeSurvivalExecutor implements CommandExecutor {
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+		if(count($args) > 0) {
+			$player = $sender->getServer()->getPlayerByPrefix($args[0]);
+			if($player instanceof Player){
+				if($player->getGamemode() === GameMode::SURVIVAL()) {
+					return true;
+				}else{
+					$player->setGamemode(GameMode::SURVIVAL());
+				}
+			}else{
+				$sender->sendMessage($args[0]." §cis not online");
+			}
+		}else{
+			if($sender instanceof Player){
+				if($sender->getGamemode() === GameMode::SURVIVAL()){
+					return true;
+				}else{
+					$sender->setGamemode(GameMode::SURVIVAL());
+				}
+			}else{
+				$sender->sendMessage("Fuck off");
+			}
+		}
+		return true;
+	}
+}
